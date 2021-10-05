f8fb363822b91a79b6fe4fccf1f7fc9309b51036
KatIsCoding <fabrigoru@gmail.com>
2021-10-05 16:13:56 -0600

    Linters corrections

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import './style.css';
 
 const addScore = (data) => {
-    const table = document.getElementById("leaderboardTable")
-    const newScore = document.createElement("li")
-    newScore.innerText = data.get("name") + ": " + data.get("score")
-    table.append(newScore)
-
-}
-
+  const table = document.getElementById('leaderboardTable');
+  const newScore = document.createElement('li');
+  newScore.innerText = `${data.get('name')}: ${data.get('score')}`;
+  table.append(newScore);
+};
 
 window.onload = () => {
-    document.getElementById("addNewScore").addEventListener("submit", (e) => {
-        addScore(new FormData(e.target))
-        e.preventDefault()
-    })
-}
+  document.getElementById('addNewScore').addEventListener('submit', (e) => {
+    addScore(new FormData(e.target));
+    e.preventDefault();
+  });
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,28 @@
-const path = require("path")
-const HtmlWebpackPlugin = require("html-webpack-plugin")
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: "./src/index.js",
-    mode: "development",
-    output: {
-        filename: "main.js",
-        path: path.resolve(__dirname, "dist")
-    },
-    devServer: {
-        static: './dist'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader']
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: "Leaderboard",
-            template: "./src/index.html"
-        })
-    ]
-}
+  entry: './src/index.js',
+  mode: 'development',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    static: './dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Leaderboard',
+      template: './src/index.html',
+    }),
+  ],
+};
