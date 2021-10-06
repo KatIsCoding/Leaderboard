6381a9b7df9d078ef7d4c0959cca12d475c71081
KatIsCoding <fabrigoru@gmail.com>
2021-10-06 15:46:21 -0600

    Fixed bug of refreshing multiple times + Linters corrections

--- src/gameAPI.js
+++ src/gameAPI.js
@@ -1,47 +1,48 @@
-const GAMEID = "2XrRdmsDhup1wio5IuwU"
-const BASEURL = "https://us-central1-js-capstone-backend.cloudfunctions.net/api/"
-import { loadingState, addScore } from "./index.js"
+import { loadingState, addScore } from './index.js';
 
+const GAMEID = '2XrRdmsDhup1wio5IuwU';
+const BASEURL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/';
+let LOADING = false
 
 export const postNewScore = async (username, score) => {
-    fetch(BASEURL + "games/"+GAMEID+"/scores/", {
-        method: 'POST',
-        body: JSON.stringify({
-            "user": username,
-            "score": score
-        }),
-        headers: {'Content-type': 'application/json; charset=UTF-8'}
-    }).then(res => {
-        // Using this with Bootstrap for a toast notification later on
-        if (res.status == 200){
-            return true
-        } else {
-            return false
-        }
-        
-    })
-}
-
-const renderTasks = (success=false, data=[]) => {
-    if (success){
-        data.forEach((el) => {
-            el.get = (key) => {return el[key]}
-            addScore(el, false)
-        })
+  fetch(`${BASEURL}games/${GAMEID}/scores/`, {
+    method: 'POST',
+    body: JSON.stringify({
+      user: username,
+      score,
+    }),
+    headers: { 'Content-type': 'application/json; charset=UTF-8' },
+  }).then((res) => {
+    // Using this with Bootstrap for a toast notification later on
+    if (res.status === 200) {
+      return true;
     }
-}
+    return false;
+  });
+};
+
+const renderTasks = (success = false, data = []) => {
+  if (success) {
+    data.forEach((el) => {
+      el.get = (key) => el[key];
+      addScore(el, false);
+    });
+  } else {
+
+  }
+};
 
 export const getScores = async () => {
-    await loadingState()
-    fetch(BASEURL + "games/"+GAMEID+"/scores/").then(res => {
-    if (res.status === 200){
-        res.json().then(json => renderTasks(true, json["result"]))
-    } else {
-        renderTasks(false, [])
+  await loadingState();
+  if (!LOADING){
+        LOADING = true
+    fetch(`${BASEURL}games/${GAMEID}/scores/`).then((res) => {
+      if (res.status === 200) {
+        res.json().then((json) => renderTasks(true, json.result));
+      } else {
+        renderTasks(false, []);
+      }
+      LOADING = false
+    });
     }
-    })
-    
-    
-}
-
-
+};
--- src/index.js
+++ src/index.js
@@ -1,31 +1,36 @@
 import './style.css';
 import { postNewScore, getScores } from './gameAPI.js';
 
-const table = document.getElementById('leaderboardTable');
+export const table = document.getElementById('leaderboardTable');
+const LOADMSG = document.createElement("p")
+LOADMSG.innerText = "Loading"
 
-export const addScore = (data, postData=true) => {
-  if (table.innerHTML ==="<p>Loading</p>") {
-    table.innerHTML = ""
+
+export const addScore = (data, postData = true) => {
+  const tableArr = [].slice.call(table.children)
+  if (tableArr.includes(LOADMSG)) {
+    table.innerHTML = '';
   }
   const newScore = document.createElement('li');
   newScore.innerText = `${data.get('user')}: ${data.get('score')}`;
   table.append(newScore);
-  if (postData){
-    postNewScore(data.get("user"), data.get("score"))
+  if (postData) {
+    postNewScore(data.get('user'), data.get('score'));
   }
 };
 
 export const loadingState = async () => {
-  table.innerHTML = "<p>Loading</p>"
-}
+  table.innerHTML = ""
+  table.appendChild(LOADMSG)
+};
 
 window.onload = () => {
-  getScores() 
+  getScores();
   document.getElementById('addNewScore').addEventListener('submit', (e) => {
     addScore(new FormData(e.target));
     e.preventDefault();
   });
-  document.getElementById("refreshLeaderboard").addEventListener("click", (e) => {
-    getScores()
-  })
+  document.getElementById('refreshLeaderboard').addEventListener('click', () => {
+    getScores();
+  });
 };
