fdf0c8dd332603718402783ed3a6c194a97bf5b8
KatIsCoding <fabrigoru@gmail.com>
2021-10-06 13:53:16 -0600

    Basic Functionality of Adding and getting scores

--- src/index.js
+++ src/index.js
@@ -1,15 +1,31 @@
 import './style.css';
+import { postNewScore, getScores } from './gameAPI.js';
 
-const addScore = (data) => {
-  const table = document.getElementById('leaderboardTable');
+const table = document.getElementById('leaderboardTable');
+
+export const addScore = (data, postData=true) => {
+  if (table.innerHTML ==="<p>Loading</p>") {
+    table.innerHTML = ""
+  }
   const newScore = document.createElement('li');
-  newScore.innerText = `${data.get('name')}: ${data.get('score')}`;
+  newScore.innerText = `${data.get('user')}: ${data.get('score')}`;
   table.append(newScore);
+  if (postData){
+    postNewScore(data.get("user"), data.get("score"))
+  }
 };
 
+export const loadingState = async () => {
+  table.innerHTML = "<p>Loading</p>"
+}
+
 window.onload = () => {
+  getScores() 
   document.getElementById('addNewScore').addEventListener('submit', (e) => {
     addScore(new FormData(e.target));
     e.preventDefault();
   });
+  document.getElementById("refreshLeaderboard").addEventListener("click", (e) => {
+    getScores()
+  })
 };
